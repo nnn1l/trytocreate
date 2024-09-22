0fb7bf2a0a20e3025f8e3611cc40d66f05630c97
Viktoria Verkholat <peremoga2009@gmail.com>
2024-09-22 21:25:35 +0300

    homework 2

--- Homework_2/ur2_hw_task3.py
+++ Homework_2/ur2_hw_task3.py
@@ -1,5 +1,5 @@
 #Here we are trying to print capital letters "O" and "H" in two print() command.
-#The first one is for letter "O"
+#The first one is for letter "O".
 print("#########\n#\t\t#\n#\t\t#\n#\t\t#\n#########\n")
 #The second one is for "H".
 print("#\t\t#\n#\t\t#\n#########\n#\t\t#\n#\t\t#")
