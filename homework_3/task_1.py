b76b31e6fa9ffd1746b98d27799940de9f9a2c48
Viktoria Verkholat <peremoga2009@gmail.com>
2024-09-23 22:39:12 +0300

    homework 3

---
+++ homework_3/task_1.py
@@ -0,0 +1,8 @@
+#The greeting program
+
+#My name is Viktoria
+name = 'Viktoria'
+#Today is Tuesday
+day = 'Tuesday'
+
+print('Good day, %s ! %s is perfect day to learn some python.' %(name, day))
---
+++ homework_3/task_3.py
@@ -0,0 +1,7 @@
+#Today we are going to call to memory about math!
+print("Today we are going to call to memory about math!")
+a = int(input("Write what is equal for A: "))
+b = int(input("Write what is equal for B: "))
+print("A + B =", a+b, '\nA - B =', a-b,
+      '\nA * B =', a*b, '\nA / B =', a/b,
+      '\nA % B =', a%b, '\nA ** B =', a**b, '\nA // B =', a//b)
