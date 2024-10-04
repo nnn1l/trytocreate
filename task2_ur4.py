e9bad95b30b56bd476be2fda3019a568654d86b1
Viktoria Verkholat <peremoga2009@gmail.com>
2024-10-04 21:27:47 +0300

    homework 4

--- task2_ur4.py
+++ task2_ur4.py
@@ -1,4 +1,4 @@
-#The valid phone number program.
+#The valid phone number program
 
 #Make a program that checks if a string is in the right format for a phone number.
 # The program should check that the string contains only numerical characters and is only 10 characters long.
