067f90e90b8ab7d9f3c3acce22c7d59da46c02c1
Viktoria Verkholat <peremoga2009@gmail.com>
2024-10-14 20:54:17 +0300

    homework 4 edit

--- task1_ur4.py
+++ task1_ur4.py
@@ -1,7 +1,7 @@
 #String manipulation.
 
 #Write a Python program to get a string made of the first 2 and the last 2 chars from a given string.
-# If the string length is less than 2, return instead of the empty string.
+# If the string length is less than 2, return instead of the empty string
 
 text_example = input('Write an example of text: ')
 amount_chars = len(text_example)
